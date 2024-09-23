411068aacb4995c97eda0df01b113b72f2c501b1
kiran <kiranugale239@gmail.com>
2024-09-23 22:37:37 +0530

    .

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,8 @@
 import localFont from "next/font/local";
 import "./globals.css";
 
+import CommonLayout from "@/components/common-layout";
+
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
   variable: "--font-geist-sans",
@@ -23,7 +25,7 @@ export default function RootLayout({ children }) {
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <CommonLayout>{children}</CommonLayout>
       </body>
     </html>
   );
