0d2cfb7b4ed27158a7f39539a3a8e399679f9faf
kiran <kiranugale239@gmail.com>
2024-09-23 22:34:52 +0530

    add User model

---
+++ src/model/User.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function (v) {
+        return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(v);
+      },
+      message: (props) => `Invalid Email !`,
+    },
+  },
+  password: String,
+  otp: String,
+  expiration: String,
+  verifyUser: {
+    type: Boolean,
+    default: false,
+  },
+  token: String,
+});
+
+const User = mongoose.models.User || mongoose.model("User", UserSchema);
+module.exports = User;
