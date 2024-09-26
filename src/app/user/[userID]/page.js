65e7f89d87e05f3f390f521c5d5eb700d3e7093f
kiran <kiranugale239@gmail.com>
2024-09-26 22:55:43 +0530

    add user page to see particular buddy profile

---
+++ src/app/user/[userID]/page.js
@@ -0,0 +1,22 @@
+import { currentUser, fetchOneBuddy, fetchUser } from "@/actions";
+import BuddyProfileCard from "@/components/buddy-profile";
+import { redirect } from "next/navigation";
+
+export default async function UserProfile({ params }) {
+  const { userID } = params;
+  const user = await currentUser();
+  if (!user) redirect("/sign-up");
+  const ProfileUser = await fetchUser(user?.userId);
+  if (user && !ProfileUser?._id) {
+    redirect("/onboard");
+  }
+
+  const SearchBuddy = await fetchOneBuddy(userID);
+  console.log(SearchBuddy);
+
+  return (
+    <>
+      <BuddyProfileCard ProfileUser={ProfileUser} SearchBuddy={SearchBuddy} />
+    </>
+  );
+}
