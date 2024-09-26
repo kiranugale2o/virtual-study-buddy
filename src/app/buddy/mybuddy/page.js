2255b58c74b201f9c609808ca37189a063f07ac5
kiran <kiranugale239@gmail.com>
2024-09-26 20:17:21 +0530

    add mybuddy component to see my matched Buddy

---
+++ src/app/buddy/mybuddy/page.js
@@ -0,0 +1,18 @@
+import { currentUser, fetchUser, getMatchedStudents } from "@/actions";
+import MyBuddyCard from "@/components/my-buudy-card";
+import { redirect } from "next/navigation";
+
+export default async function MyBuddyPage() {
+  const user = await currentUser();
+  if (!user) redirect("/sign-up");
+
+  const ProfileUser = await fetchUser(user?.userId);
+  console.log(user);
+  if (user && !ProfileUser?._id) {
+    redirect("/onboard");
+  }
+
+  const data = await getMatchedStudents(ProfileUser?._id);
+
+  return <MyBuddyCard matchedBuddy={data} />;
+}
