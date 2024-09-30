3b0fe2518f226cabcb0e2a549619521a81b8e510
kiran <kiranugale239@gmail.com>
2024-09-30 22:27:54 +0530

    add notes model

---
+++ src/model/Notes.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const NotesSchema = new mongoose.Schema({
+  senderId: {
+    type: mongoose.Schema.ObjectId,
+    required: true,
+    ref: "Student",
+  },
+  title: String,
+  content: String,
+  img: String,
+  video: String,
+  postingTime: String,
+});
+
+export const Notes =
+  mongoose.models.Notes || mongoose.model("Notes", NotesSchema);
