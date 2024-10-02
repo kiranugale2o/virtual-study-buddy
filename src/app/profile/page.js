fed661fed39eb319dd8bdb16968a1a805a3b87d0
kiran <kiranugale239@gmail.com>
2024-10-02 22:30:15 +0530

    add Profile

---
+++ src/app/profile/page.js
@@ -0,0 +1,17 @@
+import { currentUser, fetchUser, GetMyPostedNotes } from "@/actions";
+import ProfilePage from "@/components/Profile";
+import { redirect } from "next/navigation";
+
+export default async function Profile() {
+  const user = await currentUser();
+  if (!user) redirect("/sign-up");
+
+  const ProfileUser = await fetchUser(user?.userId);
+  console.log(user);
+  if (user && !ProfileUser?._id) {
+    redirect("/onboard");
+  }
+
+  const myNotes = await GetMyPostedNotes(ProfileUser?._id);
+  return <ProfilePage ProfileUser={ProfileUser} myNotes={myNotes} />;
+}
