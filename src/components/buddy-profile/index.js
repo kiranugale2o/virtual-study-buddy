1ba0cecf3557b3ab807584dd11c97b6bb4f3a975
kiran <kiranugale239@gmail.com>
2024-10-02 22:33:19 +0530

    theme Change

--- src/components/buddy-profile/index.js
+++ src/components/buddy-profile/index.js
@@ -6,7 +6,7 @@ export default function BuddyProfileCard({ ProfileUser, SearchBuddy }) {
         <div className="layout-content-container flex flex-col max-w-[960px] flex-1">
           <div className="flex p-4 @container">
             <div className="flex w-full flex-col gap-4 @[520px]:flex-row @[520px]:justify-between @[520px]:items-center">
-              <div className="flex gap-4">
+              <div className="flex gap-2">
                 <div
                   className="bg-center bg-no-repeat aspect-square bg-cover rounded-full min-h-32 w-32"
                   style={{
@@ -14,22 +14,22 @@ export default function BuddyProfileCard({ ProfileUser, SearchBuddy }) {
                   }}
                 />
                 <div className="flex flex-col justify-center">
-                  <p className=" text-[22px] text-sky-200 font-bold leading-tight tracking-[-0.015em]">
+                  <p className=" text-[22px]  font-bold leading-tight tracking-[-0.015em]">
                     {SearchBuddy?.fullName}
                   </p>
-                  <p className="text-[#636e88] text-sky-100 text-base font-normal leading-normal">
+                  <p className="text-[#636e88]  text-base font-normal leading-normal">
                     {SearchBuddy?.email}
                   </p>
-                  <p className="text-[#636e88] text-sky-100 text-base font-normal leading-normal">
+                  <p className="text-[#636e88]  text-base font-normal leading-normal">
                     {SearchBuddy?.CurrentEducation}
                   </p>
                 </div>
               </div>
               <div className="flex w-full px-3 lg:max-w-[480px] gap-3 lg:@[480px]:w-auto">
-                <button className="flex  cursor-pointer items-center justify-center overflow-hidden rounded-xl h-10 px-4 bg-sky-400 hover:bg-white hover:text-black text-black text-sm font-bold leading-normal tracking-[0.015em] flex-1 @[480px]:flex-auto">
+                <button className="flex  cursor-pointer items-center justify-center overflow-hidden rounded-xl h-10 px-4 bg-sky-400 hover:bg-black hover:text-white text-black text-sm font-bold leading-normal tracking-[0.015em] flex-1 @[480px]:flex-auto">
                   <span className="truncate">Send message</span>
                 </button>
-                <button className="flex  cursor-pointer items-center justify-center overflow-hidden rounded-xl h-10 px-4 bg-sky-400 text-white hover:bg-white hover:text-black text-black text-sm font-bold leading-normal tracking-[0.015em] flex-1 @[480px]:flex-auto">
+                <button className="flex  cursor-pointer items-center justify-center overflow-hidden rounded-xl h-10 px-4 bg-sky-400 text-black hover:bg-black hover:text-white text-black text-sm font-bold leading-normal tracking-[0.015em] flex-1 @[480px]:flex-auto">
                   <span className="truncate">Video Call</span>
                 </button>
               </div>
@@ -68,7 +68,7 @@ export default function BuddyProfileCard({ ProfileUser, SearchBuddy }) {
           <h2 className=" text-[22px] font-bold leading-tight tracking-[-0.015em] px-5 pb-3 pt-5">
             Bio
           </h2>
-          <p className=" text-[18px] flex flex-wrap w-[350px] mx-auto lg:mx-0 lg:w-full  font-normal leading-normal  pb-3 pt-1 lg:px-4">
+          <p className=" text-[18px] flex flex-wrap whitespace-pre-wrap w-[350px] mx-auto lg:mx-0 lg:w-full  font-normal leading-normal  pb-3 pt-1 lg:px-4">
             {SearchBuddy?.fullName} is a {SearchBuddy?.CurrentEducation} student
             at {SearchBuddy?.college}, with a strong interest in programming
             languages like{" "}
