54a4733d3bf95ebea89b306790fa05f653ee9c91
KIRAN UGALE <141510294+kiranugale2o@users.noreply.github.com>
2024-10-04 19:35:57 +0530

    Update database

--- src/database/index.js
+++ src/database/index.js
@@ -3,28 +3,47 @@ mongoose.set("strictPopulate", false);
 async function DatabaseConn() {
   const Mongo_DB = process.env.DATABASE_LINK;
 
-  mongoose
-    .connect(Mongo_DB, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-      console.log("Database connected successfully.");
-    })
-    .catch((error) => {
-      console.error("Error connecting to database:", error);
+   const mongoose = require('mongoose');
+
+let isConnected; // Track the connection status
+
+
+    if (isConnected) {
+        return; // Use existing connection
+    }
+
+    await mongoose.connect(Mongo_DB, {
+        useNewUrlParser: true,
+        // No need for useUnifiedTopology
     });
+    isConnected = true; // Set connection status
 
-  // Optional: Check connection status with Mongoose event listeners
-  mongoose.connection.on("connected", () => {
-    return;
-    console.log("Mongoose connected to the database.");
-  });
 
-  mongoose.connection.on("error", (err) => {
-    console.error(`Mongoose connection error: ${err}`);
-  });
 
-  mongoose.connection.on("disconnected", () => {
-    console.log("Mongoose disconnected from the database.");
-  });
+  
+  
+  // mongoose
+  //   .connect(Mongo_DB, { useNewUrlParser: true, useUnifiedTopology: true })
+  //   .then(() => {
+  //     console.log("Database connected successfully.");
+  //   })
+  //   .catch((error) => {
+  //     console.error("Error connecting to database:", error);
+  //   });
+
+  // // Optional: Check connection status with Mongoose event listeners
+  // mongoose.connection.on("connected", () => {
+  //   return;
+  //   console.log("Mongoose connected to the database.");
+  // });
+
+  // mongoose.connection.on("error", (err) => {
+  //   console.error(`Mongoose connection error: ${err}`);
+  // });
+
+  // mongoose.connection.on("disconnected", () => {
+  //   console.log("Mongoose disconnected from the database.");
+  // });
 }
 
 export default DatabaseConn;
